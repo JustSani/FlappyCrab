1ad06247f662c950b895bbfc896e59af08a04318
Sanino Fabio <fabiosanino@gmail.com>
2023-04-25 22:57:29 +0200

    Vibration working

--- www/flappyBird.js
+++ www/flappyBird.js
@@ -38,9 +38,6 @@ var gravity = 0.7;
 var score = 0;  
   
 
-
-document.addEventListener("click",moveUp);  
-  
 function moveUp(){  
     bY -= 30;
     fly.play();  
@@ -54,58 +51,67 @@ pipe[0] = {
     y : 0  
 };  
 
+$(document).ready(function(){
+    document.addEventListener("click",moveUp);  
+  
+    draw();
+   
+})
+
+
+
 function draw(){  
 
-     //cvs.height - fg.height);  
+    //cvs.height - fg.height);  
 
-    ctx.drawImage(bg,0,0);  
-    for(var i = 0; i < pipe.length; i++){  
-          
-        constant = pipeNorth.height+gap;  
-        ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);  
-        ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);  
-               
-        pipe[i].x--;  
-          
-        if( pipe[i].x == 125 ){  
-            pipe.push({  
-                x : cvs.width,  
-                y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height  
-            });   
-        }  
-          
-        if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){ 
-            //location.reload();
-            death = true;
-            // morte
+   ctx.drawImage(bg,0,0);  
+   for(var i = 0; i < pipe.length; i++){  
+         
+       constant = pipeNorth.height+gap;  
+       ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);  
+       ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);  
+              
+       pipe[i].x--;  
+         
+       if( pipe[i].x == 125 ){  
+           pipe.push({  
+               x : cvs.width,  
+               y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height  
+           });   
+       }  
+         
+       if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){ 
+           //location.reload();
+           death = true;
+           // morte
+           navigator.vibrate(3000)
 
-        }  
-          
-        if(pipe[i].x == 5){  
-            score++;  
-            scor.play();  
-        }  
-          
-          
-    }  
-    ctx.drawImage(fg,0,0) 
-    //ctx.drawImage(fg1,0,cvs.height - fg.height + 20);  
 
-    
-    
-    
-      
-    ctx.drawImage(bird,bX,bY);  
-      
-    bY += gravity;  
-      
-    ctx.fillStyle = "#000";  
-    ctx.font = "20px Verdana";  
-    ctx.fillText("Score : "+score,10, 0);  
-    
-    if(!death)
-        requestAnimationFrame(draw);  
-      
-}  
-  
-draw();  
+       }  
+         
+       if(pipe[i].x == 5){  
+           score++;  
+           scor.play();  
+       }  
+         
+         
+   }  
+   ctx.drawImage(fg,0,0) 
+   //ctx.drawImage(fg1,0,cvs.height - fg.height + 20);  
+
+   
+   
+   
+     
+   ctx.drawImage(bird,bX,bY);  
+     
+   bY += gravity;  
+     
+   ctx.fillStyle = "#000";  
+   ctx.font = "20px Verdana";  
+   ctx.fillText("Score : "+score,10, 0);  
+   
+   if(!death)
+       requestAnimationFrame(draw);  
+     
+}   
